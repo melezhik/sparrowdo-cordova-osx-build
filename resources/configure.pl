02439d3cc9ac6010b1138fe929739d52170d10d0
Alexey Melezhik <melezhik@gmail.com>
2018-10-15 14:52:20 -0500

    fix env var

--- resources/configure.pl
+++ resources/configure.pl
@@ -5,7 +5,7 @@ use strict;
 use File::Copy;
 
 my $root_dir = $ENV{BUILD_SOURCESDIRECTORY};
-my $source_code_branch = $ENV{BUILD_SOURCEBRACHNAME};
+my $source_code_branch = $ENV{BUILD_SOURCEBRANCHNAME};
 
 my $target = 'ios';
 
